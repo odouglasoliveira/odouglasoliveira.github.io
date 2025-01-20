0cb3b201387622141b8a3967e46210f0054c9ebd
Douglas Oliveira <doglasolivera13@gmail.com>
2025-01-20 10:38:54 -0300

    updates no texto e ajustes de responsividade

--- index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-  const fadeIns = document.querySelectorAll('.fade-in');
-
-  fadeIns.forEach(function(fadeIn) {
-    fadeIn.classList.add('fade-in-active');
-  });
-});
